82e96e13c16fdd04a2d416661d8a363507f4c817
Abdelrahman Reyad <abdelrahman.e7e@gmail.com>
2026-04-29 23:10:56 +0300

    feat: setting up the new nexjs project to held the frontend logic

---
+++ apps/web/.next/static/chunks/app/layout-6c56b82379ac5c42.js
@@ -0,0 +1,32 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [177],
+  {
+    8349: (e, a, l) => {
+      (Promise.resolve().then(l.t.bind(l, 3844, 23)),
+        Promise.resolve().then(l.t.bind(l, 3727, 23)),
+        Promise.resolve().then(l.t.bind(l, 4105, 23)));
+    },
+    4105: () => {},
+    3844: (e) => {
+      e.exports = {
+        style: { fontFamily: "'Inter', 'Inter Fallback'", fontStyle: "normal" },
+        className: "__className_f367f3",
+        variable: "__variable_f367f3",
+      };
+    },
+    3727: (e) => {
+      e.exports = {
+        style: {
+          fontFamily: "'Outfit', 'Outfit Fallback'",
+          fontStyle: "normal",
+        },
+        className: "__className_ed3508",
+        variable: "__variable_ed3508",
+      };
+    },
+  },
+  (e) => {
+    var a = (a) => e((e.s = a));
+    (e.O(0, [219, 555, 379, 358], () => a(8349)), (_N_E = e.O()));
+  },
+]);
