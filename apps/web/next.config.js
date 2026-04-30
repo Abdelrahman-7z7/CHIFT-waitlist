e0ed8155df4077ce9c35092b10b88aa10f6a5dfc
Abdelrahman Reyad <abdelrahman.e7e@gmail.com>
2026-04-30 13:41:39 +0300

    chore: general mantinance over the package and the staging deploying issue for the web repo

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
+  async rewrites() {
+    const API_URL = process.env.BACKEND_API_URL || "http://localhost:8787";
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
