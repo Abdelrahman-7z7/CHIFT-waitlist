d994a6c5f74c0cf2cde8fbc2dd66bb1e5fb532e4
Abdelrahman Reyad <abdelrahman.e7e@gmail.com>
2026-05-01 22:46:56 +0300

    chore: update to the schema and the drizzle config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/schema.ts",
-  out: "./migrations",
-  dialect: "sqlite",
+  schema: "./src/schema.ts", //we are telling drizzle to look at this file for the schema.
+  out: "./migrations", // where to store the migrations.
+  driver: "d1", // the driver we are using.
 });
--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,8 +1,27 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+import { sqliteTable, text, integer, check } from "drizzle-orm/sqlite-core";
 
-export const users = sqliteTable("users", {
-  id: integer("id").primaryKey(),
-  name: text("name").notNull(),
-  email: text("email").notNull().unique(),
-  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
-});
+export const subscribers = sqliteTable(
+  "subscribers",
+  {
+    id: integer("id").primaryKey(),
+    email: text("email").notNull().unique(),
+    createdAt: integer("created_at", { mode: "timestamp" })
+      .notNull()
+      .default(new Date()),
+    updatedAt: integer("updated_at", { mode: "timestamp" })
+      .notNull()
+      .default(new Date()),
+    trafficSource: text("traffic_source"),
+    device: text("device"),
+    emailVerified: integer("email_verified", { mode: "timestamp" }),
+    unsubscribedAt: integer("unsubscribed_at", { mode: "timestamp" }),
+    confirmationToken: text("confirmation_token"),
+  },
+  (table) => ({
+    emailCheck: check("email_check", sql`${table.email} LIKE '%@%.%'`),
+  }),
+);
+
+export type NewSubscriber = typeof subscribers.$inferInsert;
+export type Subscriber = typeof subscribers.$inferSelect;
